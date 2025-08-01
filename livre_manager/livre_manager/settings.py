6533a15c3c12d36286eb88d4318d091db6014fff
sm-miki <smmiki.ab@kaien-t3.net>
2025-08-01 13:56:40 +0900

    update: SECRET_KEYを秘匿化し、環境変数で管理

--- livre_manager/livre_manager/settings.py
+++ livre_manager/livre_manager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# .envファイルのパスを指定
+env = environ.Path(__file__) - 3
+environ.Env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#)xwqxqrk*ullgp$ppld)7+c7&34x=kicmc2-rquq)phmts*c'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
